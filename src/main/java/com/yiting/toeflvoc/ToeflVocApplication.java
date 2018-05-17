d4d738790f7bc4c8228d889780905175df3549d8
Yiting <shenyiting2018@gmail.com>
2018-05-17 15:55:48 -0700

    Start the project

---
+++ src/main/java/com/yiting/toeflvoc/ToeflVocApplication.java
@@ -0,0 +1,12 @@
+package com.yiting.toeflvoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeflVocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeflVocApplication.class, args);
+	}
+}
