ea4f1418ff839a2c59caf6da0334f60fb0c48982
Yiting <shenyiting2018@gmail.com>
2018-06-17 14:30:20 -0700

    Change the schema name

--- src/main/resources/sql/2018/05/2018-05-17-17-31-create-root.sql
+++ src/main/resources/sql/2018/05/2018-05-17-17-31-create-root.sql
@@ -1,7 +1,7 @@
 CREATE TABLE root(
 	id int NOT NULL AUTO_INCREMENT,
 	root_string CHAR(255) NOT NULL,
-	meaning VARCHAR(20000) NOT NULL,
+	meanings VARCHAR(20000) NOT NULL,
 	UNIQUE KEY(root_string),
 	PRIMARY KEY(id)
 ) ENGINE INNODB 
